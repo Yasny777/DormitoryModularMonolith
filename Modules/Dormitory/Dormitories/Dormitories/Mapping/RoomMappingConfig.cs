077c3603b1e4a55f8bd86d7a9ecef7323727eb5e
Yasny777 <jjasnowski01@gmail.com>
2024-11-27 22:28:41 +0100

    mapper for RoomDto

---
+++ Modules/Dormitory/Dormitories/Dormitories/Mapping/RoomMappingConfig.cs
@@ -0,0 +1,14 @@
+﻿using Dormitories.Dormitories.Models;
+using Dormitory.Contracts.Dto;
+
+namespace Dormitories.Dormitories.Mapping;
+
+public class RoomMappingConfig : IRegister
+{
+    public void Register(TypeAdapterConfig config)
+    {
+        config.NewConfig<Room, RoomDto>()
+            .Map(dest => dest.IsAvailable, src => src.IsAvailable())
+            .Map(dest => dest.TotalOccupants, src => src.TotalOccupants);
+    }
+}
