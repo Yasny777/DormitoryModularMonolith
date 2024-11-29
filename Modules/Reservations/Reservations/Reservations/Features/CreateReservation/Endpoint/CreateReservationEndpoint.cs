48f233a20f311c14c06426c426fd38db5c68164e
Yasny777 <jjasnowski01@gmail.com>
2024-11-29 19:47:24 +0100

    cleanup namespace

--- Modules/Reservations/Reservations/Reservations/Features/CreateReservation/Endpoint/CreateReservationEndpoint.cs
+++ Modules/Reservations/Reservations/Reservations/Features/CreateReservation/Endpoint/CreateReservationEndpoint.cs
@@ -1,26 +1,20 @@
-﻿using System.Security.Claims;
-using MediatR;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Routing;
-using Reservations.Reservations.Features.CreateReservation.Handler;
-using Shared.Exceptions;
-using Shared.Features.Endpoints;
-
-namespace Reservations.Reservations.Features.CreateReservation.Endpoint;
+﻿namespace Reservations.Reservations.Features.CreateReservation.Endpoint;
 
 public class CreateReservationEndpoint : PrefixedCarterModule
 {
     public override void AddRoutes(IEndpointRouteBuilder app)
     {
         app.MapPost("/reservation", async (HttpContext httpContext, CreateReservationRequest request, ISender sender) =>
-        {
-            var userId = httpContext.User.Claims
-                             .FirstOrDefault(claim => claim.Type == ClaimTypes.NameIdentifier)?.Value
-                         ?? throw new BadRequestException("User doesn't exist");
+            {
+                var userId = httpContext.User.Claims
+                                 .FirstOrDefault(claim => claim.Type == ClaimTypes.NameIdentifier)?.Value
+                             ?? throw new BadRequestException("User doesn't exist");
 
-            var command = new CreateReservationCommand(request.RoomId, Guid.Parse(userId));
-            var result = await sender.Send(command);
-            return Results.Created();
-        }).WithTags("Reservation");
+                var command = new CreateReservationCommand(request.RoomId, Guid.Parse(userId));
+                var result = await sender.Send(command);
+                return Results.Created();
+            })
+            .WithTags("Reservation")
+            .RequireAuthorization();
     }
 }
