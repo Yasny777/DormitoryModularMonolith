adb30ecc36ed809032071fdaa3c4b6fd8e32f09f
Yasny777 <jjasnowski01@gmail.com>
2024-11-29 19:47:40 +0100

    Cancel Aggregate method with domain event

--- Modules/Reservations/Reservations/Reservations/Models/Reservation.cs
+++ Modules/Reservations/Reservations/Reservations/Models/Reservation.cs
@@ -15,16 +15,28 @@ public class Reservation : Aggregate<Guid>
 
     public static Reservation Create(Guid id, Guid roomId, Guid userId)
     {
-
-
         var reservation = new Reservation()
         {
             Status = ReservationStatus.Confirmed,
+            StartDate = new DateTime(2024, 2, 1), //! todo hard coded, should be get from db
+            EndDate =  new DateTime(2024, 6, 30),
             RoomId = roomId,
             UserId = userId,
         };
 
         reservation.AddDomainEvent(new ReservationCreatedEvent(reservation));
         return reservation;
+    }
+
+
+    public void Cancel()
+    {
+        if (Status == ReservationStatus.Cancelled)
+            throw new InvalidOperationException("Reservation is already canceled.");
+
+        Status = ReservationStatus.Cancelled;
+
+        // Emit Domain Event
+        AddDomainEvent(new ReservationCancelledEvent(this));
     }
 }
