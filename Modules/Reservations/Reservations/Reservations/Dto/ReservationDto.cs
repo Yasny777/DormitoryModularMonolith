b75599ddd5ba84c96940a1ff2007c251236fad42
Yasny777 <jjasnowski01@gmail.com>
2024-11-30 23:46:43 +0100

    RoomInfo to reservationDto

--- Modules/Reservations/Reservations/Reservations/Dto/ReservationDto.cs
+++ Modules/Reservations/Reservations/Reservations/Dto/ReservationDto.cs
@@ -2,4 +2,11 @@ using Reservations.Reservations.ValueObjects;
 
 namespace Reservations.Reservations.Dto;
 
-public record ReservationDto(Guid Id, Guid RoomId, Guid UserId, DateTime StartDate, DateTime EndDate, ReservationStatus Status);
+public record ReservationDto(
+    Guid Id,
+    Guid RoomId,
+    Guid UserId,
+    DateTime StartDate,
+    DateTime EndDate,
+    ReservationStatus Status,
+    RoomInfo RoomInfo);
