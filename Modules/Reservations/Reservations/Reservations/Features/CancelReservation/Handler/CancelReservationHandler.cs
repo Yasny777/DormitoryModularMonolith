35ff809c94ccf0a63a19722e08d0108e460aca31
Yasny777 <jjasnowski01@gmail.com>
2024-11-30 13:55:32 +0100

    Added decrement counter in redis database for distributed locking

--- Modules/Reservations/Reservations/Reservations/Features/CancelReservation/Handler/CancelReservationHandler.cs
+++ Modules/Reservations/Reservations/Reservations/Features/CancelReservation/Handler/CancelReservationHandler.cs
@@ -1,23 +1,42 @@
-﻿using Reservations.Reservations.Features.CancelReservation.Endpoint;
+﻿using Microsoft.Extensions.Caching.Distributed;
+using Reservations.Reservations.Features.CancelReservation.Endpoint;
 using Shared.Contracts.CQRS;
 
 namespace Reservations.Reservations.Features.CancelReservation.Handler;
 
-public class CancelReservationHandler(ReservationDbContext reservationDbContext)
+public class CancelReservationHandler(ReservationDbContext reservationDbContext, IDistributedCache redis)
     : ICommandHandler<CancelReservationCommand, CancelReservationResult>
 {
     public async Task<CancelReservationResult> Handle(CancelReservationCommand request,
         CancellationToken cancellationToken)
     {
-        var reservation =
-            await reservationDbContext.Reservations.FirstOrDefaultAsync(r => r.Id == request.ReservationId,
-                cancellationToken);
+        using var transaction = await reservationDbContext.Database.BeginTransactionAsync(cancellationToken);
 
-        if (reservation == null) throw new NotFoundException("Reservation", request.ReservationId);
+        try
+        {
+            var reservation =
+                await reservationDbContext.Reservations.FirstOrDefaultAsync(r => r.Id == request.ReservationId,
+                    cancellationToken);
 
-        reservation.Cancel();
+            if (reservation == null) throw new NotFoundException("Reservation", request.ReservationId);
 
-        await reservationDbContext.SaveChangesAsync(cancellationToken);
+            reservation.Cancel();
+
+            await reservationDbContext.SaveChangesAsync(cancellationToken);
+            await transaction.CommitAsync(cancellationToken);
+
+            // after commit need to decrease in redis db too
+            var redisKeyOccupants = $"room:{reservation.RoomId}:occupants";
+            var roomOccupantsCount = await redis.GetStringAsync(redisKeyOccupants, cancellationToken);
+            var newOccupantsCount = int.Parse(roomOccupantsCount);
+            newOccupantsCount -= 1;
+            await redis.SetStringAsync(redisKeyOccupants, newOccupantsCount.ToString(), cancellationToken);
+        }
+        catch (Exception e)
+        {
+            await transaction.RollbackAsync(cancellationToken);
+            throw;
+        }
 
         return new CancelReservationResult(true);
     }
